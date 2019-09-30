63ca650b1acd066c7696e2a597978d949d495d6c
Sharon Watkins <swatkins876@gmail.com>
2019-09-30 17:36:27 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -33,7 +33,8 @@ class Song
     self.all.detect { |song| song.name == song_name}
   end 
   
-  def self.find_or_create_by_name
+  def self.find_or_create_by_name(song_name)
+    self.find_by_name(song_name) || self.create_by_name(song_name)
   end 
   
   def self.alphabetical
