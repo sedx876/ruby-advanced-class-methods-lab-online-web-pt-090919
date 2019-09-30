5f23581b9a7ab90c193086f3f30edc953c9d5682
Sharon Watkins <swatkins876@gmail.com>
2019-09-30 17:25:06 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,5 +9,34 @@ class Song
   def save
     self.class.all << self
   end
+  
+  def self.create(name)
+    @@all << self.new
+  end
+  
+  def self.new_by_name
+  end 
+  
+  def self.create_by_name
+  end 
+  
+  def self.find_by_name
+  end 
+  
+  def self.find_or_create_by_name
+  end 
+  
+  def self.alphabetical
+  end 
+  
+  def self.new_from_filename
+  end 
+  
+  def self.create_from_filename
+  end 
+  
+  def self.destroy_all
+    self.all.clear
+  end 
 
 end
