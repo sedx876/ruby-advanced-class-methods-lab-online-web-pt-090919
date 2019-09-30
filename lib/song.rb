64ff7726ba8147e3d6646016c6b476df0c6acf43
Sharon Watkins <swatkins876@gmail.com>
2019-09-30 17:39:54 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -43,8 +43,8 @@ class Song
   
   def self.new_from_filename(file_name)
     new_song = self.new
-    new_song.name = file_name.split(" - ")[1].split(".")[0]
-    new_song.artist_name = file_name.split(" - ")[0]
+    new_song.name = file_name.split("  ")[1].split(".")[0]
+    new_song.artist_name = file_name.split("  ")[0]
     new_song
   end 
   
